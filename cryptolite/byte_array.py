2bf58ac2e8f1061d4e304ce19dc3f6472b3c2f0a
David Carboni <david@carboni.io>
2017-03-05 02:36:27 +0000

    Implementation of byte array handling.

---
+++ cryptolite/byte_array.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+""" This module provides the ability to convert Strings, Base-64 and hexadecimal to a byte array and vice versa.
+
+ Cryptography is mainly about manipulating byte arrays, so this class provides
+ the translations you need:
+
+ - Plain-text strings need to be converted to a byte array for encryption
+   and, after decryption, need to be converted from a byte array back to a
+   String. This is done using {@value #ENCODING} encoding.
+ - Encrypted byte arrays look like random bytes, which means they can't be
+   reliably represented as a String. The best way to represent arbitrary bytes
+   as a String is using Base-64. This class lets you convert a byte array of
+   encrypted data to Base-64 so it can be easily stored and back again so it can
+   be decrypted
+ - Finally, this class also allows you to transform a byte array to a
+   hexadecimal String and back again. This is most useful in development when
+   you need to print out values to see what's going on. Conversion from
+   hexadecimal to byte array is occasionally useful, but chances are you'll use
+   byte[] to hex most of the time.
+"""
+
+import binascii
+import base64
+
+__author__ = "David Carboni"
+
+"""The encoding to use for string operations."""
+ENCODING = "UTF8"
+
+
+def to_hex_string(byte_array):
+    """Renders the given byte array as a hex String.
+
+     This is a convenience method useful for checking values during development.
+
+     Internally, this checks for null and then calls the Apache commons-codec
+     method {@link Hex#encodeHexString(byte[])}.
+
+     :param byte_array: The byte array to be rendered.
+     :return: A hex string representation of the byte array.
+     """
+    result = None
+    if byte_array is not None:
+        result = binascii.hexlify(byte_array).decode(ENCODING)
+    return result
+
+
+def from_hex_string(hex_string):
+    """Converts the given hex string to a byte array.
+
+    :param hex_string: The hex String to parse. If it starts with 0x, this will be
+                 ignored automatically.
+    :return: A byte array, as parsed from the given String
+    """
+    result = None
+    if hex_string is not None:
+        if len(hex_string) > 1 and hex_string[1] == "x":
+            hex_string = hex_string[2:]
+        result = bytearray.fromhex(hex_string)
+    return result
+
+
+def to_base64_string(byte_array):
+    """Encodes the given byte array as a base-64 String.
+
+      Internally, this checks for null and then calls the Apache commons-codec
+      method base64.b64encode(bytetarray).
+
+      :param byte_array: The byte array to be encoded.
+      :return: The byte array encoded using base-64.
+     """
+    result = None
+    if byte_array is not None:
+        result = base64.b64encode(byte_array)
+    return result
+
+
+def from_base64_string(base64_string):
+    """Decodes the given base-64 string into a byte array.
+
+     :param base64_string: A base-64 encoded string.
+     :return: The decoded byte array.
+     """
+    result = None
+    if base64_string is not None:
+        result = base64.b64decode(base64_string)
+    return result
+
+
+def from_string(string):
+    """Converts the given String to a byte array using :ENCODING:.
+
+      :param string: The String to be converted to a byte array.
+      :return: A byte array representing the String."""
+    result = None
+    if string is not None:
+        result = bytearray(string.encode(ENCODING))
+    return result
+
+
+def to_string(byte_array):
+    """Converts the given byte array to a String using :ENCODING:.
+
+      :param byte_array: The byte array to be converted to a String.
+      :return: The String represented by the given bytes.
+      """
+    result = None
+    if byte_array is not None:
+        result = byte_array.decode(ENCODING)
+    return result
---
+++ tests/test_byte_array.py
@@ -0,0 +1,112 @@
+import unittest
+from unittest import TestCase
+from cryptolite import byte_array
+
+
+class TestByteArray(TestCase):
+
+    def setUp(self):
+        self.data = bytearray("Mary had a little Café".encode("UTF8"))
+
+    def test_hex(self):
+        """Verifies a byte array can be correctly converted to a hex String and back again."""
+
+        # Given
+        # The byte array from setup
+
+        # When
+        # We convert to hex and back again
+        hex_string = byte_array.to_hex_string(self.data)
+        back_again = byte_array.from_hex_string(hex_string)
+
+        # Then
+        # The end result should match the input
+        self.assertEqual(self.data, back_again)
+
+    def test_hex_prefix(self):
+        """Verifies that a hex String with a 0x prefix can be correctly converted to bytes."""
+
+        # Given
+        # A hex string with a 0x prefix
+        hex_string = "0x" + byte_array.to_hex_string(self.data)
+
+        # When
+        # We attempt to convert to a byte array
+        actual = byte_array.from_hex_string(hex_string)
+
+        # Then
+        # No error should occur
+        self.assertEqual(self.data, actual)
+
+    def test_hex_none(self):
+        """Verifies that None is gracefully handled."""
+
+        # When
+        # We attempt conversion
+        b = byte_array.to_hex_string(None)
+        s = byte_array.from_hex_string(None)
+
+        # Then
+        # No error should occur and we should have None results
+        self.assertEqual(None, b)
+        self.assertEqual(None, s)
+
+    def test_base64(self):
+        """Verifies a byte array can be correctly converted to base64 and back again."""
+
+        # Given
+        # The byte array from setup
+
+        # When
+        # We convert to hex and back again
+        base64_string = byte_array.to_base64_string(self.data)
+        back_again = byte_array.from_base64_string(base64_string)
+
+        # Then
+        # The end result should match the input
+        self.assertEqual(self.data, back_again)
+
+    def test_base64_none(self):
+        """Verifies that None is gracefully handled."""
+
+        # When
+        # We attempt conversion
+        b = byte_array.to_base64_string(None)
+        s = byte_array.from_base64_string(None)
+
+        # Then
+        # No error should occur and we should have None results
+        self.assertEqual(None, b)
+        self.assertEqual(None, s)
+
+    def test_string(self):
+        """Verifies a byte array can be correctly converted to a string and back again."""
+
+        # Given
+        # The byte array from setup
+
+        # When
+        # We convert to string and back again
+        string = byte_array.to_string(self.data)
+        back_again = byte_array.from_string(string)
+
+        # Then
+        # The end result should match the input
+        self.assertEqual(self.data, back_again)
+
+    def test_string_none(self):
+        """Verifies that None is gracefully handled."""
+
+        # When
+        # We attempt conversion
+        b = byte_array.to_string(None)
+        s = byte_array.from_string(None)
+
+        # Then
+        # No error should occur and we should have None results
+        self.assertEqual(None, b)
+        self.assertEqual(None, s)
+
+
+if __name__ == '__main__':
+    unittest.main()
