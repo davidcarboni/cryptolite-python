92beae196b9cca9f3064aaf43280a951e8509fad
David Carboni <david@carboni.io>
2018-04-02 13:46:05 +0100

    Tidying up module comment.

--- cryptolite/byte_array.py
+++ cryptolite/byte_array.py
@@ -1,28 +1,33 @@
-""" This module provides the ability to convert Strings, Base-64 and hexadecimal to a byte array and vice versa.
+""" The byte_array module provides the ability to convert byte arrays to
+    Strings, Base-64 and hexadecimal and vice-versa.
 
     Cryptography is mainly about manipulating byte arrays, so this class provides
-    the translations you need:
+    the different translations you need:
 
     - Plain-text strings need to be converted to a byte array for encryption
-    and, after decryption, need to be converted from a byte array back to a
-    String.
+      and, after decryption, need to be converted from a byte array back to a
+      String.
     - Encrypted byte arrays look like random bytes, which means they can't be
-    reliably represented as a String. The best way to represent arbitrary bytes
-    as a String is using Base-64. This class lets you convert a byte array of
-    encrypted data to Base-64 so it can be easily stored and back again so it can
-    be decrypted
+      reliably represented as a String. The simplest way to represent arbitrary bytes
+      as a String is using Base-64. This class lets you convert a byte array of
+      encrypted data to Base-64 so it can be easily stored and back again so it can
+      be decrypted.
     - Finally, this class also allows you to transform a byte array to a
-    hexadecimal String and back again. This is most useful in development when
-    you need to print out values to see what's going on. Conversion from
-    hexadecimal to byte array is occasionally useful, but chances are you'll use
-    byte[] to hex most of the time.
+      hexadecimal String and back again. This is most useful in development when
+      you need to print out values to see what's going on. Conversion from
+      hexadecimal to byte array is occasionally useful, but chances are you'll use
+      byte[] to hex most of the time.
 
-    The naming convention for functions is set up from the point of a byte array.
-    For example, a byte array can go:
-     - to_hex_string
+    The naming convention for functions is set up from the point of view of
+    a byte array. For example, a byte array can go:
+
+    >>> to_hex_string
+
     and back:
-     - from_hex_string
-    The same pattern is used for each pair of methods (hex, base64 and string).
+
+    >>> from_hex_string
+
+    The same pattern is used for each pair of methods (to/from hex, base64 and string).
 """
 
 import binascii
